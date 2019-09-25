5399cf1bdf5b0cfb193b03f3af56675273370cae
jisshub <jissmon476@gmial.com>
2019-09-25 11:59:56 +0530

    created delete and edit view

--- project_crm_root/course_app/urls.py
+++ project_crm_root/course_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.course_list, name='course_list'),
+    path('courses/', views.course_list, name='course_list'),
+    path('int:<course_id>', views.delete_course, name='delete'),
+    path('int:<course2_id>', views.edit_course, name='edit'),
 ]
--- project_crm_root/course_app/views.py
+++ project_crm_root/course_app/views.py
@@ -29,4 +29,17 @@ def course_list(request):
     context = {
         'courses': courses_list
     }
-    return render(request, 'course_app/index.html', context)
+    return render(request, 'course_app/courses.html', context)
+
+
+def delete_course(request, course_id):
+    CouseModel.objects.filter(id=course_id).delete()
+    return course_list(request)
+
+
+def edit_course(request, course2_id):
+    specific = CouseModel.objects.filter(id=course2_id)
+    context = {
+        'c-spec': specific
+    }
+    return render(request, 'course_app/edit.html', context)
